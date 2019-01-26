4e78c9f0fddd8f9c2092cd4c99320cc16b988d6b
ebneazer <ebneazer@uw.edu>
2019-01-25 17:54:02 -0800

    completed execrcise

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -8,11 +8,12 @@ employees <- paste("Employee", 1:100)
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
 salary_2017 <- runif(100, 40000, 50000)
+salary_2017
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-salary_adjustments <- runif(100, -5000, 1000)
+salary_adjustments <- runif(100, -5000, 10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
@@ -34,6 +35,7 @@ View(salaries)
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
 
+
 # What was the 2018 salary of Employee 57
 salaries[57, "salary_2018"]
 
@@ -46,22 +48,27 @@ max(salaries$salary_adjustments)
 
 
 # What was the "name" of the employee who received the highest raise?
-salaries[ salaries$salary_adjustments == max(salaries$salary_adjustments), "employees"]
+salaries[salaries$salary_adjustments == max(salaries$salary_adjustments), "employees"]
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries$salary_adjustments)
 
 # What was the name of the employee who recieved largest decrease in salary?
+salaries[salaries$salary_adjustments == min(salaries$salary_adjustments), "employees"]
 
 
 # What was the average salary change?
-
+mean(salaries$salary_adjustments)
 
 # For people who did not get a raise, how much money did they lose on average?
+mean(salaries[salaries$got_raise == FALSE, "salary_adjustments"])
+
 
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
+
 # Write a .csv file of your salary data to your working directory
+write.csv(salaries, "salaries.csv", row.names = FALSE)
 
